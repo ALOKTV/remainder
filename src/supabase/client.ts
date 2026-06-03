64215584da1ca3850a2c4d0308deedebf23f0bca
ALOK-TV <alok@sutara.org>
2026-06-03 21:17:59 +0530

    Your commit message

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -10,8 +10,8 @@ export function getSupabaseClient(): SupabaseClient {
   // const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
   // const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-  const supabaseUrl = "https://hekwjkabbjjgirofksxg.supabase.co";
-  const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhla3dqa2FiYmpqZ2lyb2Zrc3hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MDg0NTYsImV4cCI6MjA5NTk4NDQ1Nn0.RotRV7PMfiYw2aNq4w96cKOaLNmvoE-Rdkih1fgbmDA";
+  const supabaseUrl = "https://a.supabase.co";
+  const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhla3dqa2FiYmpqZ2lyb2Zrc3hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MDg0NTYsImV4cCI6MjA5NTk4NDQ1Nn0.RotRV7PMfiYw2aNq4w96cKOaLNmvoE-0";
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase env values. Add EXPO_PUBLIC_SUPABASE_URL and EXPO_PUBLIC_SUPABASE_ANON_KEY to .env.');
